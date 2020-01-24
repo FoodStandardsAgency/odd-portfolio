3f5715e3c7c0c359e43cfbae40331adf059b7afd
Anna Nikiel <anna.nikiel@food.gov.uk>
2020-01-24 13:39:30 +0000

    odd leads view

--- app/queries.js
+++ app/queries.js
@@ -8,7 +8,7 @@ const q = {
 	current_projects: 	'SELECT * from latest_projects where phase != $1 order by priority_main desc, project_name',
 	completed_projects: 'SELECT * from latest_projects where phase = $1 order by timestamp desc',
 	max_id:				'SELECT max(project_id) from latest_projects',
-	oddleads:			'SELECT distinct oddlead from projects order by oddlead',
+	oddleads:			'SELECT distinct oddlead from latest_projects where phase != $1 order by oddlead',
 
 	power_bi_date_flag: 'SELECT * from powerbi_input_date_flag_v',
 	power_bi_projects_days: 'SELECT * from powerbi_projects_days_v',
@@ -26,7 +26,7 @@ module.exports = {
 	completed_projects: (text, params) => pool.query(q.completed_projects, ['completed']),
 	latest_projects: 	(text, params) => pool.query(q.latest_projects),
 	max_id:				(text, params) => pool.query(q.max_id),
-	oddleads:			(text, params) => pool.query(q.oddleads),
+	oddleads:			(text, params) => pool.query(q.oddleads, ['completed']),
 	powerbi_projects_days: (text, params) => pool.query(q.power_bi_projects_days),
 	powerbi_date_flag: 	(text, params) => pool.query(q.power_bi_date_flag),
 	powerbi_phase: 		(text, params) => pool.query(q.power_bi_phase),
